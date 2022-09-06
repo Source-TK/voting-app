2fe2e836b9ee24dc84693f08e131cd53d4bc1c92
Source-TK <nyamanekgopolo1@gmail.com>
2022-09-06 15:23:14 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React, {useState} from 'react';
+
+  function App() {
+    const [year,setYear]= useState('');
+  
+    const setValue = (e)=>{
+      
+      setYear(e.target.value);
+  
+    }
+    const calculateYear = ()=>{
+  
+      let age = 2022 - year;
+  
+      if(age>=18){
+        alert("You can vote")
+      }else{
+      alert("You can't vote")
+    }
+  
+  
+    }
+    return (
+      <div className="App">
+        <input className="text" placeholder="Enter a year" onChange={(e)=>setValue}/>
+  
+        <button onClick= {calculateYear}>Submit</button>
+     
+        
+        </div>
   );
+
 }
 
 export default App;
